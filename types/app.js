c0930286d5b1657d518a9d2cbf5ece6f0f651d3c
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-02-17 16:14:01 +0100

    tsc types

---
+++ types/app.js
@@ -0,0 +1,6 @@
+function addNumbers(n1, n2) {
+    return n1 + n2;
+}
+var n1 = 2;
+var n2 = 30;
+var result = addNumbers(n1, n2);
---
+++ types/app.ts
@@ -0,0 +1,8 @@
+function addNumbers(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const n1 = 2;
+const n2 = 30;
+
+const result = addNumbers(n1, n2);
