7d6b291f296700f3b43c6734620adbbeb476df07
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-02-21 13:26:43 +0100

    tsc: private and public class

--- classes/classes.ts
+++ classes/classes.ts
@@ -1,10 +1,26 @@
 class Department {
-  name: string;
+  public name: string;
+  private students: string[] = [];
 
   constructor(n: string) {
     this.name = n;
   }
+
+  //Method in tsc
+  describe() {
+    console.log("Department: " + this.name);
+  }
+
+  addStudents(student: string) {
+    this.students.push(student);
+  }
+
+  printNumberofStudent() {
+    console.log(this.students.length);
+  }
 }
 
 const computerScience = new Department("Computer Science");
+computerScience.addStudents("Alvin");
+computerScience.describe();
 console.log(computerScience);
