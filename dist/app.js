b5187641775241e1ccec58684404ccd3d30f5bb1
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-02-28 10:16:47 +0100

    light mode switcher

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,18 @@
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const submitBtn = document.getElementById("submit-btn");
+const lightBtn = document.getElementById("light");
+const darkBtn = document.getElementById("dark");
+const darkMode = document.getElementById("darkMode");
 submitBtn.addEventListener("click", () => {
     console.log(email.value);
+    console.log(password.value);
+});
+darkBtn.addEventListener("click", () => {
+    darkMode === null || darkMode === void 0 ? void 0 : darkMode.classList.remove("switcher");
+    darkMode === null || darkMode === void 0 ? void 0 : darkMode.classList.add("dark-theme");
+});
+lightBtn.addEventListener("click", () => {
+    darkMode === null || darkMode === void 0 ? void 0 : darkMode.classList.remove("dark-theme");
+    darkMode === null || darkMode === void 0 ? void 0 : darkMode.classList.add("switcher");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
 const email = document.getElementById("email") as HTMLInputElement;
 const password = document.getElementById("password") as HTMLInputElement;
 const submitBtn = document.getElementById("submit-btn")!;
+const lightBtn = document.getElementById("light")!;
+const darkBtn = document.getElementById("dark")!;
+const darkMode = document.getElementById("darkMode");
 
 submitBtn.addEventListener("click", () => {
   console.log(email.value);
+  console.log(password.value);
+});
+
+darkBtn.addEventListener("click", () => {
+  darkMode?.classList.remove("switcher");
+  darkMode?.classList.add("dark-theme");
+});
+
+lightBtn.addEventListener("click", () => {
+  darkMode?.classList.remove("dark-theme");
+  darkMode?.classList.add("switcher");
 });
